7313509d373f97f081fa7d6cb071b608a29c6eb1
dhgouveia2 <dhgouveia2@hotmail.com>
2020-04-30 10:18:54 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 sed -i 's/127.0.0.1/'${HOST}'/g' /gekko/web/vue/dist/UIconfig.js
 sed -i 's/localhost/'${HOST}'/g' /gekko/web/vue/dist/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' /gekko/web/vue/dist/UIconfig.js
+sed -i 's/sqlite/'${DB}'/g' /gekko/web/vue/dist/UIconfig.js
 if [[ "${USE_SSL:-0}" == "1" ]] ; then
     sed -i 's/ssl: false/ssl: true/g' /usr/src/app/web/vue/dist/UIconfig.js
 fi
